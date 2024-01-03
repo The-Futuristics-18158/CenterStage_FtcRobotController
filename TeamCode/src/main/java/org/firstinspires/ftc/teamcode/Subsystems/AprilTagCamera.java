9e0e81d46fe873de69daaae925b8d94d656deda0
Ken Nutt <ken.nutt@outlook.com>
2024-01-03 18:52:42 -0500

    Field-Oriented Drive Uploaded to git for copy&paste of sections into competition robot s/w project.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/AprilTagCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/AprilTagCamera.java
@@ -2,12 +2,16 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 
 import android.util.Size;
 import com.arcrobotics.ftclib.command.SubsystemBase;
+
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
 import org.firstinspires.ftc.teamcode.RobotContainer;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -34,11 +38,11 @@ public class AprilTagCamera extends SubsystemBase {
                 .setDrawTagOutline(true)
                 .setDrawAxes(true)
                 .setDrawCubeProjection(true)
+                .setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
                 .build();
 
-        // reduce AprilTag image processing by factor of 2
-        // should detect 2" tag from 6ft away at 22fps
-        SetAprilTagDecimation(2);
+        // set apriltag resolution decimation factor
+        SetDecimation(1);
 
         // Build the vision portal
         // set parameters,then use vision builder.
@@ -46,21 +50,65 @@ public class AprilTagCamera extends SubsystemBase {
                 .setCamera(RobotContainer.ActiveOpMode.hardwareMap.get(WebcamName.class, CamName))
                 .addProcessor(myAprilTagProcessor)
                 .setCameraResolution(new Size(640,480))
-                .enableLiveView(true)
-                .setStreamFormat(VisionPortal.StreamFormat.YUY2)
+                //.setCameraResolution(new Size(1280,720))
+                .enableLiveView(false)
+                .setStreamFormat(VisionPortal.StreamFormat.MJPEG)
                 .build();
 
-        myVisionPortal.resumeStreaming();
         // set camera exposure and gain
         // values used from example code
-        //setCameraExposure(6, 250);
-
+        //SetAutoCameraExposure();
+        setCameraExposure(2, 250);
     }
 
     /** Method called periodically by the scheduler
      * Place any code here you wish to have run periodically */
     @Override
     public void periodic() {
+
+        RobotContainer.ActiveOpMode.telemetry.addData("fps", myVisionPortal.getFps());
+
+        List<AprilTagDetection> tags =  GetCurrentAprilTagDetections();
+
+        /*// revise AprilTag decimation based on ranges
+        double minrange = 144.0;
+        for (int i=0; i<tags.size(); ++i) {
+            if (tags.get(i).ftcPose.range < minrange)
+                minrange = tags.get(i).ftcPose.range;
+        }
+        if (minrange < 12.0)
+            SetDecimation(3);
+        else if (minrange <48.0)
+            SetDecimation(2);
+        else
+            SetDecimation(1);
+*/
+
+        // look for apriltag #4
+        for (int i=0; i<tags.size(); ++i)
+        {
+            RobotContainer.ActiveOpMode.telemetry.addData("AT", tags.get(i).id );
+
+            if (tags.get(i).id == 4)
+            {
+                String str = JavaUtil.formatNumber(tags.get(i).ftcPose.x, 2) + "," +
+                        JavaUtil.formatNumber(tags.get(i).ftcPose.y, 2) + "," +
+                        JavaUtil.formatNumber(tags.get(i).ftcPose.z, 2);
+                RobotContainer.ActiveOpMode.telemetry.addData("xyz", str);
+                str = JavaUtil.formatNumber(tags.get(i).ftcPose.yaw, 2) + "," +
+                        JavaUtil.formatNumber(tags.get(i).ftcPose.pitch, 2) + "," +
+                        JavaUtil.formatNumber(tags.get(i).ftcPose.roll, 2);
+                RobotContainer.ActiveOpMode.telemetry.addData("ypr", str);
+                str = JavaUtil.formatNumber(tags.get(i).ftcPose.range, 2) + "," +
+                        JavaUtil.formatNumber(tags.get(i).ftcPose.elevation, 2) + "," +
+                        JavaUtil.formatNumber(tags.get(i).ftcPose.bearing, 2);
+                RobotContainer.ActiveOpMode.telemetry.addData("reb", str);
+
+
+                //RobotContainer.ActiveOpMode.telemetry.addData("fps", myVisionPortal.getFps());
+
+            }
+        }
 
     }
 
@@ -78,8 +126,8 @@ public class AprilTagCamera extends SubsystemBase {
         return myAprilTagProcessor.getDetections();
     }
 
-    // sets decimatino of AprilTag processing
-    public void SetAprilTagDecimation (int num) {
+    // sets decimation of AprilTag processing
+    public void SetDecimation (int num) {
         myAprilTagProcessor.setDecimation(num);
     }
 
@@ -96,8 +144,9 @@ public class AprilTagCamera extends SubsystemBase {
     /** Set the camera gain and exposure. */
     public void setCameraExposure(int exposureMS, int gain) {
 
-        // set camera gain and exposure
-        RobotContainer.ActiveOpMode.sleep(20);
+        // wait until camera in streaming mode
+        while (myVisionPortal.getCameraState()!= VisionPortal.CameraState.STREAMING)
+        {}
 
         // set exposure control to manual
         ExposureControl exposureControl = myVisionPortal.getCameraControl(ExposureControl.class);
